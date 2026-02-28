cfafb756b1bdd44e873895dce5db133ad35d7c8c
jallurividya <jallurividya2002@gmail.com>
2026-02-28 23:40:05 +0530

    pet care app

--- index.js
+++ index.js
@@ -29,7 +29,7 @@ dotenv.config()
 const app = express()
 
 app.use(cors({
-  origin: "http://localhost:5173",
+  origin: "https://pet-care-client-fawn.vercel.app/",
   credentials: true
 }));
 app.use(express.json())
